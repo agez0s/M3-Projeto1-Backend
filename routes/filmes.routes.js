8b1316161f08c4fa79e11679d7984176a759a1ed
agez0s <eduardo.cordeiro@protonmail.com>
2021-10-31 22:13:01 -0300

    Rotas

--- routes/filmes.routes.js
+++ routes/filmes.routes.js
@@ -1,20 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(express.urlencoded());
-
 const listaFilmes = [];
 
-let filmeDefault = {
-    id: Date.now(),
+let filmeDefault1 = {
+    id: 1,
     nome: "A Origem",
     imagem: "https://m.media-amazon.com/images/I/61AFbsFwh7L._AC_SX679_.jpg",
     genero: "Ação",
+    nota: "9.8",
+    assistido: true
+}
+
+let filmeDefault2 = {
+    id: 2,
+    nome: "Dunkirk",
+    imagem: "https://m.media-amazon.com/images/I/91a9Ez60pmL._AC_SL1500_.jpg",
+    genero: "Ação",
+    nota: "9.6",
+    assistido: false
+}
+
+let filmeDefault3 = {
+    id: 3,
+    nome: "Interstellar",
+    imagem: "https://http2.mlstatic.com/D_NQ_NP_692718-MLB25904741171_082017-O.jpg",
+    genero: "Ficção Científica",
     nota: "10",
     assistido: true
 }
 
-listaFilmes.push(filmeDefault)
+let filmeDefault4 = {
+    id: 4,
+    nome: "Tenet",
+    imagem: "https://www.dhresource.com/0x0/f2/albu/g9/M01/5F/6B/rBVaVV7ymemAYPpjAAf0MizyWz8093.jpg/2020-new-tenet-poster-silk-movie-art-film.jpg",
+    genero: "Aventura",
+    nota: "8.6",
+    assistido: true
+}
+
+listaFilmes.push(filmeDefault1)
+listaFilmes.push(filmeDefault2)
+listaFilmes.push(filmeDefault3)
+listaFilmes.push(filmeDefault4)
 
 
 
@@ -26,6 +54,11 @@ router.get("/:id", (req,res) =>{
     let filmeId = req.params.id;
 
     const filme = listaFilmes.find(filme => filme.id == filmeId);
+    
+    if (!filme) {
+        res.status(404).send({error: "Filme não encontrado"});
+        return;
+    }
     res.send(filme);
 })
 
@@ -34,36 +67,42 @@ router.post('/add', (req, res) => {
     // recebi o objeto da vaga para cadastar vinda do cliente (via requisicao http POST)
     const { nome, genero, imagem, nota } = req.body;
     const filme = { nome, genero, imagem, nota };
-    console.log(filme)
-    console.log("---")
-    console.log(req.body)
+    if (!filme || !filme.nome || !filme.genero || !filme.nota ) {
+        res.status(400).send({message: `Filme inválido! Preencha os campos corretamente. Enviado: ${filme} ${filme.nome} ${filme.genero} ${filme.nota}`});
+        return;
+    }
     filme.id = Date.now();
     filme.assistido = false;
     listaFilmes.push(filme)
     res.status(201).send({
-        message: 'Cadastro com sucesso',
+        message: `Filme "${filme.nome}" cadastrado com sucesso`,
         data: filme
     });
 })
 
 router.put('/editar/:id', (req, res) => {
     // o objeto que veio do front para atualizar a vaga com o id recebido
-    const vagaEdit = req.body;
+    const filmeEdit = req.body;
     // o id recebido via parametro
     const idParam = req.params.id;
     // procura o indice da vaga pre cadastrada na lista de acordo com o id recebido para atualizala
-    let index = vagas.findIndex(vaga => vaga.id == idParam);
+    let index = listaFilmes.findIndex(filme => filme.id == idParam);
+
+    if (index < 0) {
+        res.status(404).send({ error: "Filme não encontrado. Impossível editar"});
+        return;
+    }
 
     // spread operator ...
     // faz um espelho do item na lista e um espelho do objeto atualizado e junta os 2
-    vagas[index] = {
-        ...vagas[index],
-        ...vagaEdit
+    listaFilmes[index] = {
+        ...listaFilmes[index],
+        ...filmeEdit
     }
 
     res.send({
-        message: `vaga ${vagas[index].titulo} atualizada com sucesso`,
-        data: vagas[index]
+        message: `Filme ${listaFilmes[index].nome} atualizado com sucesso`,
+        data: listaFilmes[index]
     })
 })
 
